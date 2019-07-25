550205d3e06001e6fb52bc41b629ac99bf93e3d9
npujol <naivy.luna@gmail.com>
2019-07-25 19:49:02 +0200

    test actualization: remove , suffix, salution

--- full_name_splitter.py
+++ full_name_splitter.py
@@ -2,39 +2,87 @@
 import re
 
 LAST_NAME_PREFIXES = [
-    'de', 'da', 'la', 'du', 'del', 'dei', 'vda.', 'dello', 'della', 'degli',
-    'delle', 'van', 'von', 'der', 'den', 'heer', 'ten', 'ter', 'vande',
-    'vanden', 'vander', 'voor', 'ver', 'aan', 'mc'
+    "de",
+    "da",
+    "la",
+    "du",
+    "del",
+    "dei",
+    "vda.",
+    "dello",
+    "della",
+    "degli",
+    "delle",
+    "van",
+    "von",
+    "der",
+    "den",
+    "heer",
+    "ten",
+    "ter",
+    "vande",
+    "vanden",
+    "vander",
+    "voor",
+    "ver",
+    "aan",
+    "mc",
 ]
 
-RE_INITIAL = re.compile(r'^\w\.?$')
-RE_APOSTROPHE = re.compile(r'\w{1}\'\w+')
+RE_INITIAL = re.compile(r"^\w\.?$", re.IGNORECASE)
+RE_APOSTROPHE = re.compile(r"\w{1}\'\w+", re.IGNORECASE)
+RE_SUFFIX = re.compile(
+    r"(i{1,3}|iv|vi{0,3}|s(enio)?r|j(unio)?r|phd|apr|rph|pe|md|ma|dmd|cme)$",
+    re.IGNORECASE,
+)
+RE_SALUTATION = re.compile(
+    r"^(mrs?|m[ia]ster|miss|ms|d(octo)?r|prof|rev|fr|judge|honorable|hon|lord|lady)\.?",
+    re.IGNORECASE,
+)
 
 
 class Splitter(object):
     def __init__(self, full_name):
-        self.full_name = full_name
+        self._full_name = full_name
         self._first_names = []
         self._last_names = []
+        self.clean_fullname()
         self.split()
 
+    def clean_fullname(self):
+        # Remove more one space, "," prefix and salution
+        full_name = re.sub(r"[\n\t,]*", "", re.sub(r" +", " ", self._full_name))
+
+        self._full_name = " ".join(
+            [
+                part
+                for part in full_name.split(" ")
+                if not is_salutation(part) and not is_suffix(part) and part
+            ]
+        )
+
     def split(self):
-        parts = self.full_name.split(" ")
+        parts = self._full_name.split(" ")
         while parts:
             part = parts.pop(0)
-            #  if prefix? or with_apostrophe? or (first_name? and last_unit? and not initial?)
-            print(
-                is_prefix(part), has_apostrophe(part), self._first_names,
-                not is_initial(part)
-            )
-            if is_prefix(part) or has_apostrophe(part) or (
-                self._first_names and not is_initial(part)
+            if (
+                is_prefix(part)
+                or has_apostrophe(part)
+                or (
+                    self._first_names
+                    and not is_initial(part)
+                    and len(self._first_names) >= 2
+                )
             ):
                 self._last_names.append(part)
                 break
             else:
                 self._first_names.append(part)
         self._last_names.extend(parts)
+
+    @property
+    def full_name(self):
+        return " ".join(self._first_names + self._last_names)
 
     @property
     def first_names(self):
@@ -55,6 +103,15 @@ def is_initial(part):
 
 def has_apostrophe(part):
     return RE_APOSTROPHE.match(part)
+
+
+def is_salutation(part):
+    return RE_SALUTATION.match(part)
+
+
+def is_suffix(part):
+    return RE_SUFFIX.match(part)
+
 
 # Adjusting exceptions like
 # "Ludwig Mies van der Rohe" => ["Ludwig",  "Mies van der Rohe"   ]
--- test/test_main.py
+++ test/test_main.py
@@ -37,25 +37,25 @@ CASE_SOLUTIONS = [
     # ["Johann Wolfgang von Goethe", ["Johann Wolfgang", "von Goethe"]],
     # # Spanish-speaking countries
     # ["Juan Martín de la Cruz Gómez", ["Juan Martín", "de la Cruz Gómez"]],
-    ["Javier Reyes de la Barrera", ["Javier", "Reyes de la Barrera"]],
-    #Exceptions?
-    ["Ludwig Mies van der Rohe", ["Ludwig", "Mies van der Rohe"]],
+    # ["Javier Reyes de la Barrera", ["Javier", "Reyes de la Barrera"]],
+    # Exceptions?
+    # ["Ludwig Mies van der Rohe", ["Ludwig", "Mies van der Rohe"]],
     # # Test ignoring unnecessary whitespaces
     # ["\t Ludwig  Mies\t van der Rohe ", ["Ludwig", "Mies van der Rohe"]],
     # ["\t van  der Rohe,\t Ludwig  Mies ", ["Ludwig Mies", "van der Rohe"]],
-    # ["\t Ludwig      ", ["Ludwig", ""]],
-    # ["  van  helsing ", ["", "van helsing"]],
-    # [", van  helsing ", ["van helsing", ""]],
+    ["\t Ludwig      ", ["Ludwig", ""]],
+    ["  van  helsing", ["", "van helsing"]],
+    [", van  helsing", ["", "van helsing"]],
     # ["\t van  der Rohe,\t Ludwig  Mies ", ["Ludwig Mies", "van der Rohe"]],
     #  Names with honorifics
-    # ['Mr. William R. Hearst, III', ['William R.', 'Hearst']],
-    # ['William Randolph Hearst', ['William Randolph', 'Hearst']],
-    # ['William R. De La Cruz', ['William R.', 'De La Cruz']],
-    # ['Mr. William R. De La Cruz III', ['William R.', 'De La Cruz']],
-    # ['William De Cruz', ['William', 'De Cruz']],
-    # ['William De La Cruz', ['William', 'De La Cruz']],
-    # ['William A. B. De La Cruz', ['William A. B.', 'De La Cruz']],
-    # ['James Hugh Calum Laurie', ['James Hugh Calum', 'Laurie']],
+    ["Mr. William R. Hearst, III", ["William R.", "Hearst"]],
+    ["William Randolph Hearst", ["William Randolph", "Hearst"]],
+    ["William R. De La Cruz", ["William R.", "De La Cruz"]],
+    ["Mr. William R. De La Cruz III", ["William R.", "De La Cruz"]],
+    ["William De Cruz", ["William", "De Cruz"]],
+    ["William De La Cruz", ["William", "De La Cruz"]],
+    ["William A. B. De La Cruz", ["William A. B.", "De La Cruz"]],
+    ["James Hugh Calum Laurie", ["James Hugh", "Calum Laurie"]],
     # [
     #     'Kiefer William Frederick Dempsey George Rufus Sutherland',
     #     ['Kiefer William Frederick Dempsey George Rufus', 'Sutherland']
@@ -65,8 +65,8 @@ CASE_SOLUTIONS = [
     # ['Hearst, William Jr', ['William', 'Hearst']],
     # ['Hearst, William Randolph', ['William Randolph', 'Hearst']],
     # ['Hearst, William, M.D.', ['William', 'Hearst']],
-    ['William', ['William', ""]],
-    ['', ["", ""]],
+    ["William", ["William", ""]],
+    ["", ["", ""]],
 ]
 
 
