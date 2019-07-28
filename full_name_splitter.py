5dbb0b4664be78688367f45a8657eb1477283000
npujol <naivy.luna@gmail.com>
2019-07-28 09:51:59 +0200

    Clean code

--- full_name_splitter.py
+++ full_name_splitter.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import re
 
 LAST_NAME_PREFIXES = [
@@ -29,59 +28,54 @@ LAST_NAME_PREFIXES = [
     "mc",
 ]
 
+# Regular expresions
 RE_INITIAL = re.compile(r"^\w\.?$", re.IGNORECASE)
 RE_APOSTROPHE = re.compile(r"\w{1}\'\w+", re.IGNORECASE)
 RE_COMPOUSED = re.compile(r"\w+\-\w+", re.IGNORECASE)
 RE_SUFFIX = re.compile(
-    r"(i{1,3}|iv|vi{0,3}|s(enio)?r|j(unio)?r|phd|apr|rph|pe|md|ma|dmd|cme)$",
+    r",? +(i{1,3}|iv|vi{0,3}|s(enio)?r|j(unio)?r|phd|apr|rph|pe|md|ma|dmd|cme)$",
     re.IGNORECASE,
 )
 RE_SALUTATION = re.compile(
-    r"^(mrs?|m[ia]ster|miss|ms|d(octo)?r|prof|rev|fr|judge|honorable|hon|lord|lady)\.?",
+    r"^(mrs?|m[ia]ster|miss|ms|d(octo)?r|prof|rev|fr|judge|honorable|hon|lord|lady)\.?$",
     re.IGNORECASE,
 )
-RE_EXCEPTIONS = re.compile(r"^(van der|(vda\.)? de la \w+)", re.IGNORECASE)
+RE_EXCEPTIONS = re.compile(r"^(van der|(vda\.)? ?de la \w+$)", re.IGNORECASE)
+RE_SPACES = re.compile(r"\s+")
 
 
-class Splitter(object):
-    def __init__(self, full_name, cant_names=1):
+class Splitter:
+    def __init__(self, full_name):
         self._full_name = full_name
-        self._cant_names = cant_names
         self._first_names = []
         self._last_names = []
-        self.clean_fullname()
-        self.split()
+        self.split(self.tokens_fullname())
 
-    def clean_fullname(self):
-        # Remove more one space, "," prefix and salution
-        full_name = re.sub(r"[\n\t,]*", "", re.sub(r" +", " ", self._full_name))
+    def tokens_fullname(self):
+        # Remove more than one spaces and suffix
+        full_name_clean = RE_SUFFIX.sub("", RE_SPACES.sub(" ", self._full_name.strip()))
+        if "," in full_name_clean:
+            return full_name_clean.split(",")[1::-1]
+        return [part for part in full_name_clean.split(" ") if part]
 
-        self._full_name = " ".join(
-            [
-                part
-                for part in full_name.split(" ")
-                if not is_salutation(part) and not is_suffix(part) and part
-            ]
-        )
-
-    def split(self):
-        parts = self._full_name.split(" ")
-        # cant_words = len(parts)
+    def split(self, parts):
         while parts:
-            part = parts.pop(0)
-            print(is_compoused(part))
+            part = parts.pop(0).strip()
             if (
                 is_prefix(part)
                 or has_apostrophe(part)
-                or is_compoused(part)
-                or (len(self._first_names) == self._cant_names and not is_initial(part))
-                or len(self._first_names) == self._cant_names
+                or (self._first_names and not parts and not is_initial(part))
             ):
                 self._last_names.append(part)
                 break
             else:
                 self._first_names.append(part)
         self._last_names.extend(parts)
+
+        if self._first_names:
+            if is_salutation(self._first_names[0]):
+                self._first_names.pop(0)
+
         self.adjust_exceptions()
 
     def adjust_exceptions(self):
@@ -91,17 +85,15 @@ class Splitter(object):
         # "Javier Reyes de la Barrera"    => ["Javier", "Reyes de la Barrera"]
         # "Rosa María Pérez Martínez Vda. de la Cruz"
         #   => ["Rosa María", "Pérez Martínez Vda. de la Cruz"]
-        print(self._first_names, self._last_names)
         if (
             len(self._first_names) > 1
             and not is_initial(self._first_names[-1])
-            and re.match(RE_EXCEPTIONS, " ".join(self._last_names))
+            and RE_EXCEPTIONS.match(" ".join(self._last_names))
         ):
             while True:
                 self._last_names.insert(0, self._first_names.pop())
                 if len(self._first_names) <= 2:
                     break
-        print(self._first_names, self._last_names)
 
     @property
     def full_name(self):
@@ -130,11 +122,3 @@ def has_apostrophe(part):
 
 def is_salutation(part):
     return RE_SALUTATION.match(part)
-
-
-def is_suffix(part):
-    return RE_SUFFIX.match(part)
-
-
-def is_compoused(part):
-    return RE_COMPOUSED.match(part)
